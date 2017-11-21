1d156aff81fbe770e6a6d9a4d00364c5431bc483
Xiyng <xiyng.dev@gmail.com>
2017-11-21 04:01:10 +0200

    Add tests that should return an error

--- test/Spec.hs
+++ test/Spec.hs
@@ -7,6 +7,7 @@ import Lib
 
 shouldParse = runParser
 as r e = r `shouldBe` pure (e, T.empty)
+withError r e = r `shouldBe` Left [e]
 
 main :: IO ()
 main = hspec $ do
@@ -21,8 +22,14 @@ main = hspec $ do
             int `shouldParse` "13" `as` 13
         it "parses a larger negative int" $ do
             int `shouldParse` "-17" `as` (-17)
+        it "doesn't parse a word" $ do
+            int `shouldParse` "foobar" `withError` ConditionNotFulfilled
     describe "Lib.versionInfo" $ do
         it "parses version 1" $ do
             versionInfo `shouldParse` "osu file format v1" `as` Version 1
         it "parses a significantly newer version" $ do
             versionInfo `shouldParse` "osu file format v13" `as` Version 13
+        it "doesn't parse version 0" $ do
+            versionInfo `shouldParse` "osu file format v0" `withError` ConditionNotFulfilled
+        it "doesn't parse a negative version" $ do
+            versionInfo `shouldParse` "osu file format v-1" `withError` ConditionNotFulfilled
