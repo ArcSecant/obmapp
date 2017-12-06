ad839a6fdf3e1e561647ae13b388c79162b740ee
Xiyng <xiyng.dev@gmail.com>
2017-12-07 01:32:37 +0200

    Add specification for version 3 of beatmap format

---
+++ src/Obmapp/Beatmap/V3.hs
@@ -0,0 +1,66 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Obmapp.Beatmap.V3 where
+
+import qualified Data.Map as M
+import qualified Data.Text as T
+import qualified Obmapp.Beatmap as B
+
+data Beatmap = Beatmap
+    { general :: General
+    , metadata :: Metadata
+    , difficulty :: Difficulty
+    , timingPoints :: [B.TimingPoint]
+    , colours :: M.Map Int B.Colour
+    , hitObjects :: [B.HitObject] }
+    deriving (Eq, Show)
+
+instance B.Beatmap Beatmap Beatmap Beatmap Beatmap Beatmap where
+    formatVersion = const $ B.FormatVersion 3
+    general = id
+    editor = id
+    metadata = id
+    difficulty = id
+    timingPoints = timingPoints
+    colours = colours
+    hitObjects = hitObjects
+
+instance B.General Beatmap where
+    audioFileName = audioFileName . general
+    audioHash = audioHash . general
+
+instance B.Editor Beatmap where
+
+instance B.Metadata Beatmap where
+    title = title . metadata
+    artist = artist . metadata
+    creator = creator . metadata
+    version = version . metadata
+
+instance B.Difficulty Beatmap where
+    hpDrainRate = hpDrainRate . difficulty
+    circleSize = circleSize . difficulty
+    overallDifficulty = overallDifficulty . difficulty
+    sliderMultiplier = sliderMultiplier . difficulty
+    sliderTickRate = sliderTickRate . difficulty
+
+data General = General
+    { audioFileName :: Maybe T.Text
+    , audioHash     :: Maybe T.Text }
+    deriving (Eq, Show)
+
+data Metadata = Metadata
+    { title   :: Maybe T.Text
+    , artist  :: Maybe T.Text
+    , creator :: Maybe T.Text
+    , version :: Maybe T.Text }
+    deriving (Eq, Show)
+
+data Difficulty = Difficulty
+    { hpDrainRate       :: Maybe Float
+    , circleSize        :: Maybe Float
+    , overallDifficulty :: Maybe Float
+    , approachRate      :: Maybe Float
+    , sliderMultiplier  :: Maybe Float
+    , sliderTickRate    :: Maybe Float }
+    deriving (Eq, Show)
