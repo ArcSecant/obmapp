8672bba6de582af1d1d3c211e36db65479fb0824
Xiyng <xiyng.dev@gmail.com>
2017-12-11 23:47:58 +0200

    Switch to automatic spec discovery

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,15 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Main where
-
-import Test.Hspec
-
-import qualified Obmapp.ParserSpec
-import qualified Obmapp.Parser.Beatmap.V3Spec
-import qualified Obmapp.Parser.OsuSpec
-
-main :: IO ()
-main = hspec $ do
-    Obmapp.ParserSpec.spec
-    Obmapp.Parser.Beatmap.V3Spec.spec
-    Obmapp.Parser.OsuSpec.spec
+-- See https://hspec.github.io/hspec-discover.html for more information about
+-- how this work.
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
