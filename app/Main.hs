14c73176d9da4a077fa88c45aca9bdeac6a277b7
Xiyng <xiyng.dev@gmail.com>
2017-12-26 22:43:32 +0200

    Change the test app to process whole directories
    Previously, the test app processed only individual files. Now, it processes
    whole directories. So far it outputs only the summary for all the files instead
    of being able to produce individual results, but this will be fixed.

--- app/Main.hs
+++ app/Main.hs
@@ -2,23 +2,45 @@
 
 module Main where
 
+import Data.List (isSuffixOf)
 import qualified Data.Text as T
-import qualified Data.Text.IO as TIO
+import qualified Data.Text.IO as T
 import System.Environment (getArgs)
+import System.Directory.Extra (listFilesRecursive)
 import Text.Megaparsec
 
+import Obmapp.Beatmap (FormatVersion (..))
 import Obmapp.Beatmap.General
 import Obmapp.Parser.Beatmap
+import Obmapp.Parser.Osu (versionInfo)
+
+type FileResults = Maybe (Either FormatVersion Beatmap)
 
 main :: IO ()
-main = getArgs >>= sequence_ . map ((TIO.putStrLn =<<) . handleFile)
+main = do
+    files <- getArgs
+        >>= fmap (filter (".osu" `isSuffixOf`) . concat)
+         .  sequence
+         .  map listFilesRecursive
+    results <- sequence $ flip map files
+         $  \file -> T.readFile file
+        >>= \content -> pure $ handleFile file content
+    handleResults results
 
-handleFile :: FilePath -> IO T.Text
-handleFile path = do
-    content <- TIO.readFile path
-    pure . summarize path $ parseMaybe beatmap content
+handleFile :: FilePath -> T.Text -> FileResults
+handleFile path content = do
+    case parse versionInfo path content of
+        Left _ -> Nothing
+        Right version -> case parse beatmap path content of
+            Left  _ -> Just (Left version)
+            Right b -> Just (Right b)
 
-summarize :: FilePath -> Maybe Beatmap -> T.Text
-summarize path b = flip T.append (T.pack path) $ case b of
-    Nothing -> "ERROR: "
-    Just _  -> "SUCCESS: "
+handleResults :: [FileResults] -> IO ()
+handleResults = flip (.) countFailsAndOKs $ \(fails, oks) -> do
+    putStrLn $ "Successes: " ++ show oks
+    putStrLn $ "Failures: "  ++ show fails
+
+countFailsAndOKs :: [FileResults] -> (Int, Int)
+countFailsAndOKs = foldr f (0, 0) where
+    f (Just (Right _)) (fails, oks) = (fails,     oks + 1)
+    f _                (fails, oks) = (fails + 1, oks)
