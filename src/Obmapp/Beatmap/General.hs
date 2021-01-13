1a7dad661de92b27a7e36c1b0513e45b876db2b6
ArcSecant <lightlance7@gmail.com>
2021-01-12 23:10:44 -0800

    more interface

--- src/Obmapp/Beatmap/General.hs
+++ src/Obmapp/Beatmap/General.hs
@@ -24,3 +24,27 @@ instance B.Beatmap Beatmap where
         BeatmapV4 b -> V4.timingPoints b
         BeatmapV5 b -> V5.timingPoints b
         BeatmapV14 b -> V14.timingPoints b
+    colours b' = case b' of
+        BeatmapV3 b -> B.colours b
+        BeatmapV4 b -> B.colours b
+        BeatmapV5 b -> V5.colours b
+        BeatmapV14 b -> V14.colours b
+    hitObjects b' = case b' of
+        BeatmapV3 b -> V3.hitObjects   b
+        BeatmapV4 b -> V4.hitObjects   b
+        BeatmapV5 b -> V5.hitObjects   b
+        BeatmapV14 b -> V14.hitObjects b
+    
+instance B.General Beatmap where
+    audioFileName b' = case b' of
+        BeatmapV3 b -> V3.audioFileName   $ V3.general b
+        BeatmapV4 b -> V4.audioFileName   $ V4.general b
+        BeatmapV5 b -> V5.audioFileName   $ V5.general b
+        BeatmapV14 b -> V14.audioFileName $ V14.general b
+
+instance B.Difficulty Beatmap where
+    sliderMultiplier b' = case b' of
+        BeatmapV3 b -> V3.sliderMultiplier   $ V3.difficulty b
+        BeatmapV4 b -> V4.sliderMultiplier   $ V4.difficulty b
+        BeatmapV5 b -> V5.sliderMultiplier   $ V5.difficulty b
+        BeatmapV14 b -> V14.sliderMultiplier $ V14.difficulty b
